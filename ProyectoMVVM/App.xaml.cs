bac09785dd36afea025a856cb98fa7eb1e238b40
Josue Riera <josueriera23@hotmail.com>
2025-01-21 16:38:46 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
